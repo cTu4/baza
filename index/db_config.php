f939640627e06b001cba1fb2d8d9821b3678c201
Aleksey <chudik9898@mail.ru>
2019-03-14 14:00:40 +0300

    copy

---
+++ index/db_config.php
@@ -0,0 +1,9 @@
+<?php
+$dbHost = "eu-cdbr-west-02.cleardb.net";
+$dbDatabase = "videos";
+$dbPasswrod = "5cdb7958d707d91";
+$dbUser = "b39b86847ed026";
+
+$dbconn = mysqli_connect($dbHost, $dbUser, $dbPasswrod, $dbDatabase);
+
+?>
