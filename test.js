0fe52a39c3a0acb7115a036f425c64dd0e02cc62
Aleksey <chudik9898@mail.ru>
2019-05-04 12:03:03 +0300

    dd

---
+++ test.js
@@ -0,0 +1,15 @@
+Ext.onReady(function () {
+    xtype: 'tabpanel',
+        activeTab: 0,
+        items: [{
+        title: 'Tab 1',
+        html: 'This is tab 1 content.'
+    },{
+        title: 'Tab 2',
+        html: 'This is tab 2 content.'
+    },{
+        title: 'Tab 3',
+        html: 'This is tab 3 content.'
+    }]
+});
+
---
+++ test_only.php
@@ -0,0 +1,15 @@
+<html>
+<head> <!-- Техническая информация о документе -->
+    <meta charset="UTF-8"> <!-- Определяем кодировку символов документа -->
+    <title>...</title> <!-- Задаем заголовок документа -->
+    <link rel="stylesheet" type="text/css" href="style.css"> <!-- Подключаем внешнюю таблицу стилей -->
+    <script src="test.js"></script> <!-- Подключаем сценарии -->
+    <script type="text/javascript" src="ext-all-debug.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+
+</head>
+<body> <!-- Основная часть документа -->
+<div id="tabpanel"></div>
+
+</body>
+</html>
