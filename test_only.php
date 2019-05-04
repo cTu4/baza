e5800c8cf5a8f8d8cfc803e337ed5f24a5116050
Aleksey <chudik9898@mail.ru>
2019-05-04 12:12:35 +0300

    dddaa

--- test_only.php
+++ test_only.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8"> <!-- Определяем кодировку символов документа -->
     <title>...</title> <!-- Задаем заголовок документа -->
     <link rel="stylesheet" type="text/css" href="style.css"> <!-- Подключаем внешнюю таблицу стилей -->
-    <script src="test.js"></script> <!-- Подключаем сценарии -->
+    <script type="text/javascript" src="test.js"></script>
     <script type="text/javascript" src="ext-all-debug.js"></script>
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
