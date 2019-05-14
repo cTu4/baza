6b374862681224b5dd758662125ebe6924e67503
Aleksey <chudik9898@mail.ru>
2019-05-14 22:42:04 +0300

    reliase 1.2

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
 $dbHost = "eu-cdbr-west-02.cleardb.net";
-$dbDatabase = "heroku_16503ce43a6aad8";
+$dbDatabase = "videocards";
 $dbPasswrod = "5cdb7958d707d91";
 $dbUser = "b39b86847ed026";
 
