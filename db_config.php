aa6a7739808fda2e04a53428c551a944b7823fe6
Aleksey <chudik9898@mail.ru>
2019-05-14 22:57:44 +0300

    reliase 1.7777

--- db_config.php
+++ db_config.php
@@ -1,8 +1,7 @@
 <?php
 $host = "eu-cdbr-west-02.cleardb.net";
 $database = "heroku_16503ce43a6aad8";
-$passwrod = "5cdb7958d707d91";
+$password = "5cdb7958d707d91";
 $user = "b39b86847ed026";
 
 $mysqli = new mysqli($host, $user, $password,$database);
-?>
